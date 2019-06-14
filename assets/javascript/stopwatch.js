d8d6b3a089d3f819bc3e1002bb1d6c55cfc9b04c
soheila <soheilalotfi@ymail.com>
2019-06-14 14:44:35 -0400

    the clock starts running when the user presses the start key

---
+++ assets/javascript/stopwatch.js
@@ -0,0 +1,46 @@
+var time=0;
+var countLap=1;
+var isClockRunning=false;
+var intervalId;
+
+$("#start").click(start);
+$("#stop").click(stop);
+$("#reset").click(reset);
+$("#lap").click(recordLap);
+
+
+
+function  start(){
+    if(!isClockRunning){
+        intervalId =setInterval(count,1000);
+        isClockRunning= true;
+}
+}    
+
+function count(){
+
+    time++;
+    var converted=timeConverter(time);
+    $("#display").text(converted);
+
+}
+
+    
+function timeConverter(t) {
+
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+    seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+    minutes = "00";
+    }
+    else if (minutes < 10) {
+    minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+}
